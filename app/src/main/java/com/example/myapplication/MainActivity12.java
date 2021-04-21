f5f3cdaafdcff89dd23aa35aefdc1ddb5350e0b8
CodeChenL <2540735020@qq.com>
2021-04-21 08:39:26 +0700

    优化逻辑

--- app/src/main/java/com/example/myapplication/MainActivity12.java
+++ app/src/main/java/com/example/myapplication/MainActivity12.java
@@ -25,21 +25,14 @@ public class MainActivity12 extends AppCompatActivity {
         Intent intent=new Intent(MainActivity12.this,MainActivity13.class);
         Bundle bundle= new Bundle();
         EditText editText=(EditText)findViewById(R.id.sg);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                sex=checkedId;
-                System.out.println(sex);
-            }
-        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 bundle.putString("sg","您的身高为"+editText.getText().toString());
-                if (sex==R.id.n){
+                if (radioGroup.getCheckedRadioButtonId()==R.id.n){
                     bundle.putString("sex","您是一位男士");
                     bundle.putString("tz","您的标准体重为"+String.valueOf((Integer.parseInt(editText.getText().toString())-80)*0.7)+"公斤");
-                }else if (sex==R.id.v){
+                }else if (radioGroup.getCheckedRadioButtonId()==R.id.v){
                     bundle.putString("sex","您是一位女士");
                     bundle.putString("tz","您的标准体重为"+String.valueOf((Integer.parseInt(editText.getText().toString())-70)*0.6)+"公斤");
                 }
